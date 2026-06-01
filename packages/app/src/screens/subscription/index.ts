297c2f7e2a00349cbb8bf53b552e863acfeddbb7
Antoine de Veyrac <antoine.2vey@gmail.com>
2026-06-01 17:55:30 +0200

    app(paywall): add Apple-compliant auto-renewable subscription disclosure (#43)

--- packages/app/src/screens/subscription/index.ts
+++ packages/app/src/screens/subscription/index.ts
@@ -1,4 +1,3 @@
-export { RedeemGiftCodeSheet } from './RedeemGiftCodeSheet'
 export { SubscriptionCancelScreen } from './SubscriptionCancelScreen'
 export { SubscriptionPayScreen } from './SubscriptionPayScreen'
 export { SubscriptionUsageScreen } from './SubscriptionUsageScreen'
